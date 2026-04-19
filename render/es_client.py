d5d68b5eeace3e7f814d509912e675dad698a8e8
AnshuSarkar <74862392+AnshuSarkarANX@users.noreply.github.com>
2026-04-20 00:37:58 +0530

    update variable name

--- render/es_client.py
+++ render/es_client.py
@@ -1,6 +1,6 @@
 import os
 from elasticsearch import AsyncElasticsearch
 
-ES_HOST    = os.getenv("ES_HOST", "http://localhost:9200")
+ES_HOST    = os.getenv("ELASTICSEARCH_URL", "http://localhost:9200")
 es         = AsyncElasticsearch(ES_HOST)
 INDEX_NAME = "tech_news"
